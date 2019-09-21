c903bb654cacc0ad9ff324e6c5b99970f47959fb
Alexander <alxv.oliveira@gmail.com>
2019-09-21 13:37:30 -0300

    Troca dos métodos imprime() pelo método especial do Python __str__

--- modelo.py
+++ modelo.py
@@ -19,8 +19,8 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
-    def imprime(self):
-        print(f"{self.nome} - {self.ano} - {self.likes} Likes")
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.likes} Likes"
 
 
 class Filme(Programa):
@@ -28,16 +28,16 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f"{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes")
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes"
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f"{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes")
+    def __str__(self):
+        return f"{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes"
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -52,4 +52,4 @@ atlanta.dar_likes()
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
