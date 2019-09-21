0416e01321c390c3113a8be5090fc647f5ee0992
Alexander <alxv.oliveira@gmail.com>
2019-09-21 14:31:59 -0300

    Alteração da classe Playlist para não herda nada

--- modelo.py
+++ modelo.py
@@ -40,10 +40,18 @@ class Serie(Programa):
         return f"{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes"
 
 
-class Playlist(list):
+class Playlist:
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    @property
+    def tamanho(self):
+        return len(self._programas)
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -64,9 +72,7 @@ atlanta.dar_like()
 
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
-print(f"Tamanho da Playlist: {len(playlist_fim_de_semana)}")
+print(f"Tamanho da Playlist: {len(playlist_fim_de_semana.listagem)}")
 
-for programa in playlist_fim_de_semana:
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
-
-print(f"Tá ou não tá? {demolidor in playlist_fim_de_semana}")
